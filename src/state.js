ee316236056a7f41c6b0e5c4839653be4fbdc3fb
Ross Libby <rosslibby92@gmail.com>
2025-09-17 18:36:32 -0400

    built simple state

--- src/state.js
+++ src/state.js
@@ -1,4 +1,35 @@
-export const state = {
-  score: 0,
+class State {
+  constructor(initialState = {}) {
+    this.state = initialState;
+  }
+  set(obj) {
+    this.state = { ...this.state, obj }
+  }
+  getState() {
+    return this.state;
+  }
+}
+
+const game = new State({
+  speed: 125,
   running: false,
-};
+  interval: null,
+  gameOver: false,
+  paused: false,
+});
+
+const player = new State({
+  score: 0,
+  duration: 0,
+  name: '',
+});
+
+const board = new State({
+  rows: 0,
+  cols: 0,
+  cells: [],
+  food: 0,
+  player: [],
+});
+
+export const state = { game, player, board };
