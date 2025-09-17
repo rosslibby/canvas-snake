bb24dd079bcf1da0551b4549cd6d9e51d304b602
Ross Libby <rosslibby92@gmail.com>
2025-09-17 19:50:10 -0400

    resize canvas on window resize

--- src/dom/canvas.js
+++ src/dom/canvas.js
@@ -1,12 +1,23 @@
+import { getInnerSize } from './utils';
 import { dom } from '../state';
 
 export function createCanvas() {
   const { container } = dom.state();
   const canvas = document.createElement('canvas');
 
+  dom.set({ canvas });
+
   if (!container.querySelector('canvas')) {
+    setCanvasDimensions();
     container.appendChild(canvas);
   }
 
-  dom.set({ canvas });
+  window.addEventListener('resize', setCanvasDimensions);
+}
+
+function setCanvasDimensions() {
+  const { container, canvas } = dom.state();
+  const [width, height] = getInnerSize(container);
+  canvas.setAttribute('width', width);
+  canvas.setAttribute('height', height);
 }
