65f4b169567266b713c0746c34275f06a69ff9aa
Ross Libby <rosslibby92@gmail.com>
2025-10-10 17:15:13 -0400

    set up canvas in ts

---
+++ ts-src/dom/canvas.ts
@@ -0,0 +1,33 @@
+import { getInnerSize } from './utils';
+import { dom, state as appState } from '../state';
+
+export function createCanvas() {
+  const { container } = dom.state();
+  const canvas = document.createElement('canvas');
+  dom.set({ canvas });
+
+  updateCells();
+  container.appendChild(canvas);
+  window.addEventListener('resize', updateCells);
+}
+
+function updateCells() {
+  const state = appState.state();
+  const {
+    board,
+    dom: { canvas, container },
+    config: { cellSize },
+  } = state;
+  const [width, height] = getInnerSize(container);
+  const useableWidth = width * .9;
+  const useableHeight = height * .9;
+
+  const cols = Math.floor(useableWidth / cellSize);
+  const rows = Math.floor(useableHeight / cellSize);
+
+  canvas.setAttribute('width', cols * cellSize);
+  canvas.setAttribute('height', rows * cellSize);
+
+  const cells = Array.from({ length: rows * cols }, () => 0);
+  board.update({ rows, cols, cells });
+}
