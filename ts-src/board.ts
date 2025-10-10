2019ed67044f4ac9cf123edbb9ce237698b96f8c
Ross Libby <rosslibby92@gmail.com>
2025-10-10 17:13:27 -0400

    built dropFoodmethod

---
+++ ts-src/board.ts
@@ -0,0 +1,19 @@
+import { UpdateFn } from 'types'
+import { idxFromXY } from './player'
+import { getBoard, getPlayer } from './state'
+
+export function dropFood() {
+  const { cells, update } = getBoard() as {
+    cells: number[]
+    update: UpdateFn
+  }
+  const { snake } = getPlayer()
+
+  const snakeIndices = snake.map(idxFromXY)
+  const available = cells.filter(
+    (_, i) => !snakeIndices.includes(i)
+  )
+  const foodIdx = Math.floor(Math.random() * available.length)
+  console.log(`New food drop:`, foodIdx)
+  update({ food: foodIdx })
+}
