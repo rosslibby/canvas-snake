50432020aed6105e2effbb17b5af65bb9dfd4846
Ross Libby <rosslibby92@gmail.com>
2025-10-10 17:12:59 -0400

    built renderer

---
+++ ts-src/render.ts
@@ -0,0 +1,35 @@
+import { xyFromIdx } from './player'
+import { getConfig, getPlayer, getDom, getBoard } from './state'
+
+export function render() {
+  const { canvas, ctx } = getDom()
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawPlayer()
+  drawFood()
+}
+
+function drawFood() {
+  const { food } = getBoard() as { food: number }
+
+  drawBlock(xyFromIdx(food))
+}
+
+function drawPlayer() {
+  const { snake } = getPlayer()
+  snake.map(drawBlock)
+}
+
+function drawBlock([x, y]: [number, number]) {
+  const { ctx } = getDom()
+  const { cellSize, fill, radius } = getConfig()
+  const size = cellSize - 2
+
+  x = x * cellSize
+  y = y * cellSize
+
+  ctx.beginPath()
+  ctx.fillStyle = fill
+  ctx.roundRect(x + 2, y + 2, size, size, radius)
+  ctx.fill()
+  ctx.closePath()
+}
